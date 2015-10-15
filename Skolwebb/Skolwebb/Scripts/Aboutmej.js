614fe973ffd74d41538e75cbf76c9032f307c097
MånsBergman <mans.bergman@gmail.com>
2015-10-15 10:26:44 +0200

    javascript

--- Skolwebb/Skolwebb/Scripts/Aboutmej.js
+++ Skolwebb/Skolwebb/Scripts/Aboutmej.js
@@ -7,36 +7,69 @@ $(document).ready(function () {
 
 var vad;
 var hej;
-var 
+var ting;
+var tal;
+var hardr
 vad = 0;
 hej = 0;
-tal = 0;
-function tra2div(tal, vad) {
-    hej += tal;
-    if (hej == 1 && vad == 1) {
+tal1 = 0;
+tal2 = 0;
+tal3 = 0;
+tal4 = 0;
+ting = 0;
+hardr = 0;
+//function tra2div(tal, vad, ting, hardr)
+function tra2div(tal1, hardr, tal2, tal3) {
+    vad += tal1;
+    hej += tal2;
+    ting += tal3;
+    //NAV!!!!!!!!
+    if (vad == 0 && hardr == -1) {
+        $("div.circleBase").animate({ margin: '+=7% 0px 0px 0px', height: '+=40px', width: '+=40px', borderWidth: '+=4px', backgroundSize: '+=30px' }, 1000);
+        $.removeData(hardr);
+    }
+    else if (vad == 1 && hardr == 1) {
+        $("div.circleBase").animate({ margin: '-=7% 0px 0px 0px', height: '-=40px', width: '-=40px', borderWidth: '-=4px', backgroundSize: '-=30px' }, 1000);
+        $.removeData(hardr);
+    }
 
+
+
+    //ARTICLES!!!!1
+    if (hej == 1 && hardr == 1) {
         $("article.About").fadeIn(500);
-        $("div.circleBase").animate({ margin: '-=7% 0px 0px 0px', height: '-=40px', width: '-=40px', borderWidth: '-=4px',backgroundSize: '-=30px' }, 1000);
-       
-
-        $.removeData(vad);
     }
-    else if (hej == 0 && vad == -1) {
+
+    else if (hej == 0 && hardr == -1) {
         $("article.About").fadeOut(500);
-        $("div.circleBase").animate({ margin: '+=7% 0px 0px 0px', height: '+=40px', width: '+=40px', borderWidth: '+=4px',backgroundSize: '+=30px' }, 1000);
-      
-        $.removeData(vad);
+        $("article.Projects").fadeOut(500);
     }
-    //else if()
-    //{
-    //}
-     
+    else if (ting == 1 && hardr == 1) {
+        $("article.Projects").fadeIn(500);
+    }
 
-    
-else if (hej >= 2) {
-    hej = 1
-}
-else if (hej <= -1) {
-    hej = 0
-}
+
+
+
+        //RESET!!!!!!!
+
+    else if (ting >= 2) {
+        ting = 1
+    }
+    else if (ting <= -1) {
+        ting = 0
+    }
+
+    else if (hej >= 2) {
+        hej = 1
+    }
+    else if (hej <= -1) {
+        hej = 0
+    }
+    else if (vad >= 2) {
+        vad = 1
+    }
+    else if (vad <= -1) {
+        vad = 0
+    }
 }
