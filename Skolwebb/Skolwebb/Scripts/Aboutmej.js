b999ea124fdbabe4033210d0351d5e34722bb70e
MånsBergman <mans.bergman@gmail.com>
2015-10-15 11:14:12 +0200

    javascript vidare

--- Skolwebb/Skolwebb/Scripts/Aboutmej.js
+++ Skolwebb/Skolwebb/Scripts/Aboutmej.js
@@ -3,73 +3,130 @@ $(document).ready(function () {
     $("article.About").hide();
     $("article.Contacting").hide();
     $("article.Projects").hide();
+    $("div.Menu").hide();
 });
-
-var vad;
-var hej;
-var ting;
-var tal;
-var hardr
-vad = 0;
-hej = 0;
+$(document).ready(function () {
+    $("div.Menu").fadeIn(2000);
+});
+var nav;
+var me;
+var pro;
+var con;
+//var tal1;
+//var tal2;
+//var tal3;
+//var tal4;
+var hardrnav;
+var hardA;
+var hardP;
+var hardC;
+nav = 0;
+me = 0;
+pro = 0;
+con = 0;
 tal1 = 0;
 tal2 = 0;
 tal3 = 0;
 tal4 = 0;
-ting = 0;
-hardr = 0;
+hardrnav = 0;
+hardA = 0;
+hardP = 0;
+hardC = 0;
 //function tra2div(tal, vad, ting, hardr)
-function tra2div(tal1, hardr, tal2, tal3) {
-    vad += tal1;
-    hej += tal2;
-    ting += tal3;
+function tra2div(tal1, hardrnav, hardA, hardP, hardC, tal2, tal3, tal4) {
+    nav += tal1;
+    me += tal2;
+    pro += tal3;
+    con += tal4;
     //NAV!!!!!!!!
-    if (vad == 0 && hardr == -1) {
+    if (nav == 0 && hardrnav == -1) {
         $("div.circleBase").animate({ margin: '+=7% 0px 0px 0px', height: '+=40px', width: '+=40px', borderWidth: '+=4px', backgroundSize: '+=30px' }, 1000);
-        $.removeData(hardr);
+        $.removeData(hardrnav);
+        $.removeData(hardA);
+        $.removeData(hardP);
+        $.removeData(hardC);
     }
-    else if (vad == 1 && hardr == 1) {
+    else if (nav == 1 && hardrnav == 1) {
         $("div.circleBase").animate({ margin: '-=7% 0px 0px 0px', height: '-=40px', width: '-=40px', borderWidth: '-=4px', backgroundSize: '-=30px' }, 1000);
-        $.removeData(hardr);
+        $.removeData(hardrnav);
+        $.removeData(hardA);
+        $.removeData(hardP);
+        $.removeData(hardC);
     }
 
 
 
-    //ARTICLES!!!!1
-    if (hej == 1 && hardr == 1) {
+    //ARTICLES!!!!!
+    //about
+    if (me == 1 && hardA == 1) {
         $("article.About").fadeIn(500);
+        $.removeData(hardA);
+        $.removeData(hardP);
+        $.removeData(hardC);
+        $.removeData(hardrnav);
     }
 
-    else if (hej == 0 && hardr == -1) {
+    else if (me == 0 && hardA == -1) {
         $("article.About").fadeOut(500);
-        $("article.Projects").fadeOut(500);
+        $.removeData(hardA);
+        $.removeData(hardP);
+        $.removeData(hardC);
+        $.removeData(hardrnav);
     }
-    else if (ting == 1 && hardr == 1) {
+        //about end
+        // projects
+    else if (pro == 1 && hardP == 1) {
         $("article.Projects").fadeIn(500);
+        $.removeData(hardA);
+        $.removeData(hardP);
+        $.removeData(hardC);
+        $.removeData(hardrnav);
     }
+    else if (pro == 0 && hardP == -1) {
+        $("article.Projects").fadeOut(500);
+        $.removeData(hardA);
+        $.removeData(hardP);
+        $.removeData(hardC);
+        $.removeData(hardrnav);
+    }
+        //project end
+        //Contact
 
+        //contact end
 
 
 
         //RESET!!!!!!!
+    else if (nav >= 2) {
+        nav = 1
+    }
+    else if (nav <= -1) {
+        nav = 0
+    }
+    else if (me >= 2) {
+        me  = 1
+    }
+    else if (me <= -1) {
+        me = 0
+    }
+    else if (pro >= 2) {
+        pro = 1
+    }
+    else if (pro <= -1) {
+        pro = 0
+    }
+    else if (con >= 2) {
+        con = 1
+    }
+    else if (con <= -1) {
+        con = 0
+    }
 
-    else if (ting >= 2) {
-        ting = 1
-    }
-    else if (ting <= -1) {
-        ting = 0
-    }
 
-    else if (hej >= 2) {
-        hej = 1
-    }
-    else if (hej <= -1) {
-        hej = 0
-    }
-    else if (vad >= 2) {
-        vad = 1
-    }
-    else if (vad <= -1) {
-        vad = 0
-    }
+
+
+
+
+
+
 }
